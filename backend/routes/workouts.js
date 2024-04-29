4ab8180dab98dc1599b08d89e208e20aeb4b66b2
Shuga8 <lotocharles8@gmail.com>
2024-04-29 13:26:13 +0100

    Models, Workout models

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Workout = require("../models/WorkoutModel");
 
 const router = express.Router();
 
@@ -10,7 +11,16 @@ router.get("/:id", (req, res) => {
   res.json({ msg: "Get single workout" });
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create();
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+
   res.json({ msg: "POST a new workout" });
 });
 
