56b4847b9d8f5f0c978adc0410e1a0903910130c
Shuga8 <lotocharles8@gmail.com>
2024-05-01 01:14:22 +0100

    controllerlayouts

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,24 +1,22 @@
 const express = require("express");
-const { createWorkout } = require("../controllers/workoutController");
+const {
+  createWorkout,
+  getWorkouts,
+  singleWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ msg: "Get all workouts" });
-});
+router.get("/", getWorkouts);
 
-router.get("/:id", (req, res) => {
-  res.json({ msg: "Get single workout" });
-});
+router.get("/:id", singleWorkout);
 
 router.post("/", createWorkout);
 
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "Delete a workout" });
-});
+router.delete("/:id", deleteWorkout);
 
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "Update a workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
