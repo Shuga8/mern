585413dce3cf684a917f219a69701a8133640db1
Shuga8 <lotocharles8@gmail.com>
2024-06-22 00:03:53 +0100

    Auth Context hook

---
+++ frontend/src/hooks/useAuthContext.js
@@ -0,0 +1,12 @@
+import { AuthContext } from "../Context/AuthContext";
+import { useContext } from "react";
+
+export const useWorkoutsContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw Error("use AuthContext must be used inside a AuthContextProvider");
+  }
+
+  return context;
+};
