7c6495157e44bb700dc02aec70e258e98a30b768
Shuga8 <lotocharles8@gmail.com>
2024-06-21 23:51:37 +0100

    user login, jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,17 @@ const createToken = (_id) => {
 
 // login
 const loginUser = async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 // signup
